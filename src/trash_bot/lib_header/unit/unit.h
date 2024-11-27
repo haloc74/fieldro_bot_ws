d08628e6c411a87f56284b93873bf69d8e29f010
TrashKim <kyh8301@twinny.ai>
2024-11-27 16:41:31 +0900

    1. 모든 Unit들의 Parent Class로 Unit Class 설계 2. Unit Class 선언 및 정의

---
+++ src/trash_bot/lib_header/unit/unit.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <mutex>
+#include <map>
+
+
+#include "define/unit_define.h"
+#include "define/unit_state.h"
+#include "define/unit_action_define.h"
+#include "helper/thread_action_info.h"
+#include "log/log.h"
+
+#include "trash_bot/UnitControl.h"
+
+namespace fieldro_bot
+{
+  class Unit
+  {
+  public:
+    Unit(std::string config_file, std::string session);		            // 생성자
+    
+    virtual ~Unit();		  // 소멸자
+
+    ros::NodeHandle* get_node_handle();    // 노드 핸들러 반환
+    virtual void update(){}		                                        // 업데이트
+
+    virtual bool control(std::vector<std::string> command_list){}        // 컨트롤 함수(consol input을 이용한 control)
+
+  protected:
+    std::mutex        _lock;		                      // 뮤텍스
+
+    ros::NodeHandle*   _node_handle;                   // 노드 핸들러
+    ros::AsyncSpinner* _spinner;                       // 비동기 스피너
+    ros::Subscriber   _subscribe_unit_action;         // unit action 명령을 수신하는 subscriber
+    ros::Publisher    _publish_unit_action_complete;  // unit action 수행 완료를 publish 하는   publisher
+    ros::Publisher    _publish_unit_alive;            // unit alive publish
+    int32_t           _spinner_count;                 // spinner count
+
+    UnitName          _name;                          // unit 이름
+    UnitState         _state;                         // unit 상태
+    UnitAction        _action;                        // unit action
+
+    ThreadActionInfo* _update_thread;                 // update thread 동작 정보
+    ThreadActionInfo* _alive_thread;                  // alive  thread 동작 정보
+
+    std::map<int32_t, int32_t, std::string> _command; // command map
+    
+
+  protected:
+    void publish_unit_alive();                                                  // unit alive publish
+    void destroy(){}                                                            // 객체 소멸 예약
+    void log_msg(LogLevel level, int32_t error_code, const std::string& msg);   // 로그 메시지 출력
+
+  protected:
+    virtual void subscribe_unit_action(const trash_bot::UnitControl& msg) {}    // unit action subscribe
+    virtual void publish_unit_action_complete(int32_t action, int32_t error_code);                        // unit action complete publish
+    virtual void load_option(std::string config_file, std::string session);     // option load
+  };
+
+
+
+}
---
+++ src/trash_bot/src/lib_cpp/unit/unit.cpp
@@ -0,0 +1,135 @@
+#include "unit/unit.h"
+#include "helper/helper.h"
+#include <trash_bot/UnitActionComplete.h>
+#include <trash_bot/UnitAliveMsg.h>
+
+
+namespace fieldro_bot
+{
+  Unit::Unit(std::string config_file, std::string session)
+  {
+    // main option 로드
+    load_option(config_file, session);
+
+    // command map 초기화
+    _command.clear();
+
+    // node handler 생성
+    _node_handle = new ros::NodeHandle();
+
+    // spinner 설정
+    _spinner = new ros::AsyncSpinner(_spinner_count);
+
+    // action complete publish 생성
+    _publish_unit_action_complete =
+    _node_handle->advertise<trash_bot::UnitActionComplete>("trash_bot/action_complete", 20);
+  
+    // alive publish 생성
+    _publish_unit_alive =
+    _node_handle->advertise<trash_bot::UnitAliveMsg>("trash_bot/UnitAliveMsg", 10);
+
+    // alive thread 정보 생성
+    _alive_thread = new ThreadActionInfo(config_file, "alive");
+    _alive_thread->_active = true;
+    _alive_thread->_thread = std::thread(std::bind(&Unit::publish_unit_alive, this));
+
+
+    _update_thread = nullptr;
+   
+
+    // child class에서 해야 할일
+    // 1. subscriber 생성
+    // 2. update thread 생성
+    // 3. spinner 시작
+    // 4. thread 시작
+    // 5. name, state, action 설정
+  }
+
+  Unit::~Unit()
+  {
+    // alive thread 해제
+    _alive_thread->_active = false;
+    safe_delete(_alive_thread);
+
+    // spinner 정지
+    _spinner->stop();
+    safe_delete(_spinner);
+
+    // node handler 해제
+    _node_handle->shutdown();
+    safe_delete(_node_handle);
+  }
+
+  /**
+  * @brief      unit action complete publish
+  * @param[in]  int32_t action : 동작 완료된 action
+  * @param[in]  int32_t error_code : 에러 코드
+  * @param[out] void
+  * @note       동작 완료 이후 별도의 처리가 필요할 경우 chile class에서 Unit::publish_unit_action_complete()
+  *             를 명시적으로 호출 하고 추가 처리를 하자 !!!
+  */
+  void Unit::publish_unit_action_complete(int32_t action, int32_t error_code)
+  {
+    trash_bot::UnitActionComplete msg;
+
+    msg.receive_object  = to_int(fieldro_bot::UnitName::System);
+    msg.action_object   = to_int(_name);
+    msg.complete_action = action;
+    msg.error_code      = error_code;
+
+    _publish_unit_action_complete.publish(msg);
+
+    return;
+  }
+
+  
+  void Unit::publish_unit_alive()
+  {
+    while(_alive_thread->_active)
+    {
+      // unit alive publish
+      trash_bot::UnitAliveMsg msg;
+      msg.index = to_int(_name);
+      msg.state = to_int(_state);
+      _publish_unit_alive.publish(msg);
+
+      // thread Hz 싱크 및 독점 방지를 위한 sleep
+      std::this_thread::sleep_for(std::chrono::milliseconds(_alive_thread->_sleep));
+    }
+  }
+
+  void Unit::log_msg(LogLevel level, int32_t error_code, const std::string& msg)
+  {
+    LOG->add_log(_name, level, error_code, msg);
+    return;
+  }
+
+  ros::NodeHandle* Unit::get_node_handle()
+  {
+    return _node_handle;
+  }
+
+  void Unit::load_option(std::string config_file, std::string session)
+  {
+    try
+    {
+      std::ifstream yaml_file(config_file);
+      YAML::Node yaml = YAML::Load(yaml_file);
+      yaml_file.close();
+
+      _spinner_count        = yaml["main"]["spinner_count"].as<int32_t>();
+    }
+    catch(YAML::Exception& e)
+    {
+      std::cout << "YAML Exception : " << e.what() << std::endl;
+    }
+    catch(std::exception& e)
+    {
+      std::cout << "Exception : " << e.what() << std::endl;
+    }
+    catch(...)
+    {
+      std::cout << "Unknown Exception" << std::endl;
+    }
+  } 
+}
