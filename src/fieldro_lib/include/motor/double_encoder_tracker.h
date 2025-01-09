5bfcdcbf58e02e95a40b927ec2864ff774c282a3
TrashKim <kyh8301@twinny.ai>
2025-01-09 10:34:58 +0900

    1. DoubleEncoderTracker 구현

--- src/fieldro_lib/include/motor/double_encoder_tracker.h
+++ src/fieldro_lib/include/motor/double_encoder_tracker.h
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #include <cstdint>
@@ -6,21 +5,25 @@
 
 namespace frb
 {
+  /**
+  * @brief		  traction, steering을 동시에 사용하는 motor의 
+  *             encoder 변화를 추적하기 위한 class
+  */
   class DoubleEncoderTracker
   {
   public:
     DoubleEncoderTracker();
     virtual ~DoubleEncoderTracker();
 
-    void update_traction(int32_t encoder);
-    void update_steering(int32_t encoder);
+    void    reset();                         // 데이터 초기화         
+    double* get_velocity();                  // 현재 속도 반환
 
-    double get_traction_velocity();
-    double get_steering_velocity();
-
+    void update_traction(int32_t encoder);   // traction encoder 업데이트
+    void update_steering(int32_t encoder);   // steering encoder 업데이트
+    
   protected:
-    int32_t _prev_encoder[2];
-    double  _prev_time[2];
-    double  _velocity[2];
+    int32_t _prev_encoder[2];     // 이전 encoder 값
+    double  _prev_time[2];        // 이전 측정 시간
+    double  _velocity[2];         // 현재 속도
   };
 }
---
+++ src/fieldro_lib/src/motor/double_encoder_tracker.cpp
@@ -0,0 +1,91 @@
+#include "double_encoder_tracker.h"
+#include <float.h> // Include float.h for DBL_MAX
+
+namespace frb
+{
+  DoubleEncoderTracker::DoubleEncoderTracker()
+  {
+    reset();
+  }
+
+  DoubleEncoderTracker::~DoubleEncoderTracker()
+  {
+  }
+
+  double* DoubleEncoderTracker::get_velocity()
+  {
+    return _velocity;
+  }
+
+  /**
+  * @brief      data reset
+  * @attention  _prev_time[0] == DBL_MAX
+  */
+  void DoubleEncoderTracker::reset()
+  {
+    _prev_time[0] = DBL_MAX;
+    _prev_time[1] = DBL_MAX;
+
+    _prev_encoder[0] = 0;
+    _prev_encoder[1] = 0;
+
+    _velocity[0] = 0;
+    _velocity[1] = 0;    
+  }
+
+  /**
+  * @brief      update traction velocity
+  * @param[in]  int32_t encoder : 현재 traction encoder 값
+  * @return     void
+  * @note       _prev_time[0] == DBL_MAX 판정으로 최초 데이터인지 확인
+  */
+  void DoubleEncoderTracker::update_traction(int32_t encoder)
+  {
+    auto current_time = std::chrono::steady_clock::now();
+    double current_time_ms = std::chrono::duration<double, std::milli>(current_time.time_since_epoch()).count();
+ 
+    if(_prev_time[0] == DBL_MAX)
+    {
+      _velocity[0] = 0.0;
+    }
+    else
+    {
+      double delta_time = current_time_ms - _prev_time[0];
+      _velocity[0] = static_cast<double>(encoder - _prev_encoder[0]) / delta_time;
+    }
+
+    // 최신 데이터로 update
+    _prev_time[0]     = current_time_ms;
+    _prev_encoder[0]  = encoder;
+
+    return;
+  }
+
+  /**
+  * @brief      update steering velocity
+  * @param[in]  int32_t encoder : 현재 steering encoder 값
+  * @return     void
+  * @note       _prev_time[1] == DBL_MAX 판정으로 최초 데이터인지 확인
+  */
+  void DoubleEncoderTracker::update_steering(int32_t encoder)
+  {
+    auto current_time = std::chrono::steady_clock::now();
+    double current_time_ms = std::chrono::duration<double, std::milli>(current_time.time_since_epoch()).count();
+ 
+    if(_prev_time[1] == DBL_MAX)
+    {
+      _velocity[1] = 0.0;
+    }
+    else
+    {
+      double delta_time = current_time_ms - _prev_time[1];
+      _velocity[1] = static_cast<double>(encoder - _prev_encoder[1]) / delta_time;
+    }
+
+    // 최신 데이터로 update
+    _prev_time[1]     = current_time_ms;
+    _prev_encoder[1]  = encoder;
+
+    return;
+  }
+}
